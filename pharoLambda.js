18940b2f67036ea7b97592dedb0d35f202b22fde
macta <macta@pobox.com>
2017-07-05 00:17:29 +0000

    Update pharoLambda.js - improved bash quoting to eliminate regex, and simplified exec usage

--- pharoLambda.js
+++ pharoLambda.js
@@ -7,36 +7,20 @@
 const exec = require('child_process').exec;
 
 exports.handler = function(event, context) {
-
-    const child = exec('./pharo Pharo.image --no-default-preferences eval "Lambda processJSON: \'' 
-    	+ JSON.stringify(event).replace(/"/g,'\\"') + '\'"', (error) => {
-       		context.done(error, 'Process complete!');
-    });
-
-	child.stderr.on('data', console.error);
-	
-	child.stdout.on('data', function(data) {
-		console.log(data);
-		result = JSON.parse(data);
-		context.succeed(result);
-	});
-
-}
-
-function output(text, context) {
-
-    var response = {
-        outputSpeech: {
-            type: "PlainText",
-            text: text
-        },
-        card: {
-            type: "Simple",
-            title: "Heat Genius",
-            content: text
-        },
-        shouldEndSession: true
-    };
+	const cmdLine = "./pharo Pharo.image --no-default-preferences exec 'Lambda processJSON: '\\''" 
+    	+ JSON.stringify(event) + "'\\'''";
+    	
+    console.log("exec(\"" + cmdLine + "\")");
     
-    context.succeed( { response: response } );
+    exec(cmdLine, (error, stdoutResult, stderrResult) => {
+    	if(error) {
+    		console.error(`exec error: ${error}`);
+       		context.done(error, "lambda exec failure");
+       	} else {
+       		console.log(`exec success`);
+       		const result = JSON.parse(stdoutResult);
+		
+			context.succeed({ "response": result });
+       	}
+    });
 }
